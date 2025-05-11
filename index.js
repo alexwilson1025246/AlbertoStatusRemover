cc1d2603cf1950f2f508546b1a0bbc89444cc1ec
alexwilson1025246 <jjhardy60@gmail.com>
2025-05-11 12:47:08 +0100

    index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Playing 2.0.5", "Playing 2.0.5"];
-const statusTypes = [ 'dnd', 'dnd'];
+const statusMessages = ["Playing 3.0.0", "Playing 3.0.0"];
+const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
